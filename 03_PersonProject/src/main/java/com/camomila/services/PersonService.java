15765f9f988aa2a9a1b173a84d511880ef0b29f2
camila.morais <camila.rmorais73@gmail.com>
2020-05-22 15:24:34 -0300

    Inclusão de funcionalidades usando os verbos GET, PUT, POST, DELETE

---
+++ 03_PersonProject/src/main/java/com/camomila/services/PersonService.java
@@ -0,0 +1,55 @@
+package com.camomila.services;
+
+import com.camomila.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class PersonService {
+
+    private final AtomicLong counter = new AtomicLong();
+
+    public Person create(Person person) {
+        return person;
+    }
+
+    public Person update(Person person) {
+        return person;
+    }
+
+    public void delete(String id) {
+
+    }
+
+    public Person findById(String id) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Camila");
+        person.setLastName("Morais");
+        person.setAddress("São Paulo - SP");
+        person.setGender("Female");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0 ; i < 8; i++){
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person Name" + i);
+        person.setLastName("Last Name" + i);
+        person.setAddress("Some Address in Brazil " + i);
+        person.setGender("Female");
+        return person;
+    }
+}
