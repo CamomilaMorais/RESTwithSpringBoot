a908d8e0e2e23923d90a625b04874bb44ebeb9ff
camila.morais <camila.rmorais73@gmail.com>
2020-05-24 16:15:28 -0300

    Criação de projeto à parte para implementação de Autenticação com JWT e Spring Security

---
+++ 03_PersonProject_JWT_SpringSecurity/src/main/java/com/camomila/repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.camomila.repository;
+
+import com.camomila.data.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
