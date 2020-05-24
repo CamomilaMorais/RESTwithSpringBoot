34d872215bc98cafcae37a9734188c2725cf0b18
camila.morais <camila.rmorais73@gmail.com>
2020-05-23 23:16:19 -0300

    Criação de projeto à parte para Hateoas

---
+++ 03_PersonProject_Hateoas/src/main/java/com/camomila/controller/PersonController.java
@@ -0,0 +1,45 @@
+package com.camomila.controller;
+
+import com.camomila.data.vo.v1.PersonVO;
+import com.camomila.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO findById(@PathVariable("id") Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+    public List<PersonVO> findAll() {
+        return service.findAll();
+    }
+
+    @PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+            consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO create(@RequestBody PersonVO person) {
+        return service.create(person);
+    }
+
+    @PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+            consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO update(@RequestBody PersonVO person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
