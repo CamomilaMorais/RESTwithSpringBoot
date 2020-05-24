0bbbceb51780feba8a5095461353253d4b4777ab
camila.morais <camila.rmorais73@gmail.com>
2020-05-24 01:59:31 -0300

    Adição do projeto à parte com suporte ao Swagger

---
+++ 03_PersonProject_Swagger/src/main/java/com/camomila/controller/PersonController.java
@@ -0,0 +1,60 @@
+package com.camomila.controller;
+
+import com.camomila.data.vo.v1.PersonVO;
+import com.camomila.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequestMapping("api/person/v1")
+public class PersonController {
+
+    @Autowired
+    private PersonService service;
+
+    @GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO findById(@PathVariable("id") Long id) {
+        PersonVO personVO = service.findById(id);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+        return personVO;
+    }
+
+    @GetMapping(produces = {"application/json", "application/xml", "application/x-yaml"})
+    public List<PersonVO> findAll() {
+        List<PersonVO> personsVO = service.findAll();
+        personsVO
+                .stream()
+                .forEach(
+                        p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel())
+                );
+        return personsVO;
+    }
+
+    @PostMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+            consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO create(@RequestBody PersonVO person) {
+        PersonVO personVO = service.create(person);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(person.getKey())).withSelfRel());
+        return personVO;
+    }
+
+    @PutMapping(produces = {"application/json", "application/xml", "application/x-yaml"},
+            consumes = {"application/json", "application/xml", "application/x-yaml"})
+    public PersonVO update(@RequestBody PersonVO person) {
+        PersonVO personVO = service.update(person);
+        personVO.add(linkTo(methodOn(PersonController.class).findById(person.getKey())).withSelfRel());
+        return personVO;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return ResponseEntity.ok().build();
+    }
+}
