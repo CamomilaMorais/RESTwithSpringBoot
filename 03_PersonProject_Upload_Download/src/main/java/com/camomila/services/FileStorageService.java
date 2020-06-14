d7cfbe8be0ec7f24b38b79fd81c1114b58e6547b
camila.morais <camila.rmorais73@gmail.com>
2020-06-14 18:54:14 -0300

    Implementando o Download de Arquivos

--- 03_PersonProject_Upload_Download/src/main/java/com/camomila/services/FileStorageService.java
+++ 03_PersonProject_Upload_Download/src/main/java/com/camomila/services/FileStorageService.java
@@ -2,8 +2,11 @@ package com.camomila.services;
 
 import com.camomila.config.FileStorageConfig;
 import com.camomila.exception.FileStorageException;
+import com.camomila.exception.MyFileNotFoundException;
 import com.ctc.wstx.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -37,11 +40,32 @@ public class FileStorageService {
                 throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
             }
 
+            /**
+             * Local de configuração de local onde o arquivo é armazenado (banco de dados, Amazon, Computador, etc...)
+             */
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             return fileName;
         }catch (Exception e){
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!");
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName){
+        try {
+
+            /**
+             * Local de configuração de local onde o arquivo é armazenado (banco de dados, Amazon, Computador, etc...)
+             */
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()){
+                return resource;
+            }else {
+                throw new MyFileNotFoundException("File not found " + fileName);
+            }
+        }catch (Exception e){
+            throw new MyFileNotFoundException("File not found " + fileName, e);
         }
     }
 }
